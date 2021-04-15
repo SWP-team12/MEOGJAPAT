2f132ce69d56dc78eb75e1aff9af0caac0d58598
JIEUN.HAN <53422017+undmia@users.noreply.github.com>
2021-04-16 08:18:41 +0900

    #3 UI디자인
    로그인 화면, 메인화면(네비게이션 연결), 프로필 화면 작성 중(미완).
    에러 발생하여 수정 중이나 시간이 오래 걸려 중간 결과물 push

---
+++ frontend/app/src/main/java/com/swp12/meogjapatfrontend/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package com.swp12.meogjapatfrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+
+    // 네비게이션바 연결
+
+
+
+    // 프로필 상세 내용 출력
+
+
+}
