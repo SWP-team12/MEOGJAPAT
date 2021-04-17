9fed4e1831aa8933ad9b100d0a5ed7abdcdc5612
JIEUN.HAN <53422017+undmia@users.noreply.github.com>
2021-04-17 11:30:56 +0900

    #3 UI 디자인
    로그인,  프로필
    
    로그인 - SNS연동 성공 후 회원가입 팝업(닉네임, 계좌) 생성 필요
    프로필 - 레이아웃 인식 못하는 오류 수정 필요

--- frontend/app/src/main/java/com/swp12/meogjapatfrontend/LoginActivity.kt
+++ frontend/app/src/main/java/com/swp12/meogjapatfrontend/LoginActivity.kt
@@ -8,4 +8,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    
+    // SNS 연동 성공 후 회원가입(닉네임, 계좌 입력) 팝업 생성
 }
